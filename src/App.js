46e39f922c2db8e0bae137d0620fc6d379d2d03d
louisglam <48673503+louisglam@users.noreply.github.com>
2020-10-09 21:11:33 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends React.Component{
+    // constructor(){
+    //   super();
+    //   this.state = { color : "red" };
+
+    // }
+
+    render(props){
+      return <h2>Hello,{this.props.color} App</h2>
+    }
+}
+
+ReactDOM.render(<App color="red"/>, document.getElementById('root'));
+
+
+export default App;
+
+
+
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
